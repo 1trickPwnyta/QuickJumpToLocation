6c0ff2f5d776733126e4b4463cb9c1e938d0dfae
1trickPwnyta <sarahpace37@gmail.com>
2024-03-20 10:50:43 -0700

    Update to 1.5

---
+++ 1.5/Source/QuickJumpToLocationSettings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace QuickJumpToLocation
+{
+    public class QuickJumpToLocationSettings : ModSettings
+    {
+        public static bool JumpOnly = false;
+
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+
+            listingStandard.Begin(inRect);
+
+            listingStandard.CheckboxLabeled("QuickJumpToLocation_JumpOnly".Translate(), ref JumpOnly);
+
+            listingStandard.End();
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref JumpOnly, "JumpOnly");
+        }
+    }
+}
